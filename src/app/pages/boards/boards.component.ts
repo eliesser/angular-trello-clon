e89327ac85a869c025b921f507464582ecf4c751
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-16 10:50:52 +0200

    add design to boards component

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,13 +1,27 @@
 // Angular imports
 import { Component } from '@angular/core';
 
+// Third party imports
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBox,
+  faWaveSquare,
+  faClock,
+} from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+
 // Project imports
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FontAwesomeModule],
   templateUrl: './boards.component.html',
 })
-export class BoardsComponent {}
+export class BoardsComponent {
+  faTrello = faTrello;
+  faBox = faBox;
+  faWaveSquare = faWaveSquare;
+  faClock = faClock;
+}
