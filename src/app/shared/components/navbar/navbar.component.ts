de006b06d1d68ca833b32b0d47bd9df02989af4e
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-16 10:46:14 +0200

    add fortawesome library

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,16 +2,28 @@
 import { Component } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay';
 
+// Third party imports
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBell,
+  faInfoCircle,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons';
+
 // Project imports
 import { BtnComponent } from '../btn/btn.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [BtnComponent, OverlayModule],
+  imports: [BtnComponent, OverlayModule, FontAwesomeModule],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
+  faClose = faClose;
+
   isOpen = false;
   isOpenBody = false;
 }
