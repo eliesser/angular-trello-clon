29c5ed2a71a8dc4bbb62090bfd00a55bbba0ecb3
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-16 11:01:22 +0200

    add cdk-accordion menu dynamic

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -19,11 +19,12 @@ import { faTrello } from '@fortawesome/free-brands-svg-icons';
 
 // Project imports
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule],
+  imports: [NgFor, NavbarComponent, FontAwesomeModule, CdkAccordionModule],
   templateUrl: './boards.component.html',
 })
 export class BoardsComponent {
@@ -37,4 +38,40 @@ export class BoardsComponent {
   faBorderAll = faBorderAll;
   faUsers = faUsers;
   faGear = faGear;
+
+  items = [
+    {
+      label: 'Item 1',
+      items: [
+        {
+          label: 'Sub Item 1.1',
+        },
+        {
+          label: 'Sub Item 1.2',
+        },
+      ],
+    },
+    {
+      label: 'Item 2',
+      items: [
+        {
+          label: 'Sub Item 2.1',
+        },
+      ],
+    },
+    {
+      label: 'Item 3',
+      items: [
+        {
+          label: 'Sub Item 3.1',
+        },
+        {
+          label: 'Sub Item 3.2',
+        },
+        {
+          label: 'Sub Item 3.3',
+        },
+      ],
+    },
+  ];
 }
